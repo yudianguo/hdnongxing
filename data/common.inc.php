01405759c2f1e3accfe715744102d8d0afca8842
于殿国 <yu_dianguo@163.com>
2018-03-12 19:45:40 +0800

    change

--- data/common.inc.php
+++ data/common.inc.php
@@ -1,9 +1,9 @@
 <?php
 //数据库连接信息
-$cfg_dbhost = 'localhost';
+$cfg_dbhost = '123.56.13.155';
 $cfg_dbname = 'hdnongxing';
 $cfg_dbuser = 'root';
-$cfg_dbpwd = '';
+$cfg_dbpwd = 'tianxiawudi123!@#';
 $cfg_dbprefix = 'dede_';
 $cfg_db_language = 'utf8';
 
